c1616db43d085f0064d657bd0a69ed987dc40d0f
Vladimir Solomenchuk <vovasty@aramzamzam.net>
2011-06-03 16:48:05 +0400

    improved 1-col page count

--- TestWebKit/paginate.js
+++ TestWebKit/paginate.js
@@ -19,15 +19,24 @@ function loadResource(filename, filetype){
 
 function paginate(desiredWidth, desiredHeight)
 {
+    var gap = 10;
+    var colsPerPage = 1;
     var bodyID = document.getElementsByTagName('body')[0];
-    totalHeight = bodyID.offsetHeight;
-    var pageCount = Math.floor(totalHeight/desiredHeight) + 1;
+    var totalHeight = bodyID.offsetHeight;
+    var columnCount = (Math.floor(totalHeight/desiredHeight) + 1);
+    
+//    var lastElement;
+//    
+//    for ( lastElement = bodyID.lastChild; !lastElement.offsetTop; lastElement = lastElement.previousElementSibling );
+    
     bodyID.style.paddingBottom = 10; //(optional) prevents clipped letters around the edges
-    bodyID.style.width = desiredWidth * pageCount;
+    bodyID.style.width = desiredWidth * columnCount;
     bodyID.style.height = desiredHeight;
-    bodyID.style.WebkitColumnCount = pageCount;
-    bodyID.style.WebkitColumnGap = 10;
-    return Math.floor(bodyID.scrollWidth / bodyID.clientWidth) + 1;
+    bodyID.style.WebkitColumnCount = columnCount * colsPerPage;
+    bodyID.style.WebkitColumnGap = gap;
+
+    return Math.round(bodyID.scrollWidth / bodyID.clientWidth);
+//    return Math.round(lastElement.offsetTop / bodyID.clientHeight) / colsPerPage;
 }
 
 function scrollToPosition(position)
@@ -36,3 +45,73 @@ function scrollToPosition(position)
     document.body.scrollLeft=position;
     return document.body.scrollLeft != prev;
 }
+
+function BMMarker()
+{
+
+}
+BMMarker.prototype.selectionCoords = function()
+{
+    var selection = window.getSelection();
+    console.log(selection);
+    var startNode = this.getXPath(selection.baseNode);
+    var endNode = this.getXPath(selection.extentNode);
+    console.log(startNode);
+    console.log(endNode);
+    var cs = this.getCoords(selection.baseNode.parentElement);
+    var ce = this.getCoords(selection.extentNode.parentElement);
+    console.log(cs);
+    console.log(ce);
+    
+}
+
+BMMarker.prototype.getXPath = function(node, path) 
+{
+    path = path || [];
+    if(node.parentNode) {
+        path = this.getXPath(node.parentNode, path);
+    }
+    
+    if(node.previousSibling) {
+        var count = 1;
+        var sibling = node.previousSibling
+        do {
+            if(sibling.nodeType == 1 && sibling.nodeName == node.nodeName) {count++;}
+            sibling = sibling.previousSibling;
+        } while(sibling);
+        if(count == 1) {count = null;}
+    } else if(node.nextSibling) {
+        var sibling = node.nextSibling;
+        do {
+            if(sibling.nodeType == 1 && sibling.nodeName == node.nodeName) {
+                var count = 1;
+                sibling = null;
+            } else {
+                var count = null;
+                sibling = sibling.previousSibling;
+            }
+        } while(sibling);
+    }
+    
+    if(node.nodeType == 1) {
+        path.push(node.nodeName.toLowerCase() + (node.id ? "[@id='"+node.id+"']" : count > 0 ? "["+count+"]" : ''));
+    }
+    return path;
+};
+
+BMMarker.prototype.getCoords = function(el) 
+{
+  
+  var _x = 0;
+    var _y = 0;
+    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+        _x += el.offsetLeft - el.scrollLeft;
+        _y += el.offsetTop - el.scrollTop;
+        el = el.offsetParent;
+    }
+  
+  
+  return [_x,_y];
+};
+
+var marker = new BMMarker();
